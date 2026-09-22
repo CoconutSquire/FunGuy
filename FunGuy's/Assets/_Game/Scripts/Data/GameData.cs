ede0662d8fd63afde50db92756e21dc8afb7e616
CoconutSquire <garrett7embry7@gmail.com>
2026-09-21 22:42:53 -0700

    Change GameData class to partial class

--- FunGuy's/Assets/_Game/Scripts/Data/GameData.cs
+++ FunGuy's/Assets/_Game/Scripts/Data/GameData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GameData {
+public partial class GameData {
   public StatRulesCatalog StatRules { get; private set; }
   public LevelProgressionRules LevelRules { get; private set; }
   public Dictionary<string, CharacterDef> Characters = new();
