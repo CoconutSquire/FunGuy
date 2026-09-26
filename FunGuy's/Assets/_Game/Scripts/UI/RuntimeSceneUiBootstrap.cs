754299d18650709f875a46c7e4e6ea1ad18672b7
CoconutSquire <garrett7embry7@gmail.com>
2026-09-25 21:56:11 -0700

    Rework equipment actions and coordinate screen colors

--- FunGuy's/Assets/_Game/Scripts/UI/RuntimeSceneUiBootstrap.cs
+++ FunGuy's/Assets/_Game/Scripts/UI/RuntimeSceneUiBootstrap.cs
@@ -459,10 +459,10 @@ public static class RuntimeSceneUiBootstrap
 
         var equipmentDetails = EnsureLabel(equipmentCard.transform, "Lbl_EquipmentDetails", "", 20, FontStyle.Normal, TextAnchor.MiddleCenter, SoftWhite);
         SetRect(equipmentDetails.rectTransform, CenterAnchor, CenterAnchor, new Vector2(0, -320), new Vector2(820, 120));
-        var equipmentUpgrade = EnsureButton(equipmentCard.transform, "Btn_EquipmentUpgrade", "Equip Equipment", new Vector2(0, -390), new Vector2(420, 80), IdleHuntressTheme.AccentFor(UiTone.Team), out var equipmentUpgradeLabel);
+        var equipmentUpgrade = EnsureButton(equipmentCard.transform, "Btn_EquipmentUpgrade", "Equip Equipment", new Vector2(190, -390), new Vector2(300, 80), new Color(.12f, .52f, .25f, 1f), out var equipmentUpgradeLabel);
         var charButtons=new Button[5];
         for(int i=0;i<5;i++){ var b=EnsureButton(equipmentCard.transform,$"Btn_EquipmentCharacter{i+1}","Character",new Vector2(-250+i*125,410),new Vector2(115,60),IdleHuntressTheme.AccentFor(UiTone.Team),out _); charButtons[i]=b; }
-        var equipmentBack=EnsureButton(equipmentCard.transform,"Btn_EquipmentBack","Back to Formation",new Vector2(-300,-570),new Vector2(300,80),new Color(.28f,.34f,.39f,1f),out var equipmentBackLabel);
+        var equipmentBack=EnsureButton(equipmentCard.transform,"Btn_EquipmentBack","Back to Formation",new Vector2(-190,-390),new Vector2(300,80),new Color(.28f,.34f,.39f,1f),out var equipmentBackLabel);
         choiceLabels.ToList().ForEach(x => x.color = Color.white);
         gearLabels.ToList().ForEach(x => x.color = Color.white);
         equipmentUpgradeLabel.color = Color.white;
